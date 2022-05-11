4ff0aec9cd6b5718a275712ae0678ff5b4efcfde
MaxYnovobry <94967857+MaxYnovobry@users.noreply.github.com>
2022-05-11 17:32:45 +0200

    almostfinish

--- Exercice_2/Exercice2.js
+++ Exercice_2/Exercice2.js
@@ -1,2 +1,3 @@
 
-TweenMax.to('#fire', 1, {y:200});
+TweenMax.to('.fire',5,{repeat:-1,backgroundPosition: "0px -46680px",ease:SteppedEase.config(120)});
+TweenMax.to('.ice',5,{repeat:-1,backgroundPosition: "0px -39864px",ease:SteppedEase.config(132)});
